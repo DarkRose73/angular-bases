dbc55038258a14e823509baf48c845143f495f96
Joan Salas <joan.salas.silva@gmail.com>
2023-05-04 18:14:56 -0400

    app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Mi primera app de angular';
+  public counter: number = 10;
 }
