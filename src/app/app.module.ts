3d267d8c29d58ce8f5a233d80136e10be0592c15
Joan Salas <joan.salas.silva@gmail.com>
2023-05-08 15:51:49 -0400

    angular modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.module';
+
 import { AppComponent } from './app.component';
-import { CounterComponent } from './counter/counter.component';
 import { HeroComponent } from './heroes/hero/hero.component';
 import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   // Aqui se declaran los componentes que se utilizaran en este modulo
-  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, HeroComponent, ListComponent],
+  // LOS MODULOS SIEMPRE VAN EN LOS IMPORTS
+  imports: [BrowserModule, CounterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
