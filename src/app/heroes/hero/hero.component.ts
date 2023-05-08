3a78c573ff213596825e62f534fcf0eb494c5a4c
Joan Salas <joan.salas.silva@gmail.com>
2023-05-08 14:43:10 -0400

    homework: change name & age

--- src/app/heroes/hero/hero.component.ts
+++ src/app/heroes/hero/hero.component.ts
@@ -16,4 +16,12 @@ export class HeroComponent {
   getHeroDescription(): string {
     return `${this.name} - ${this.age}`;
   }
+
+  changeHero(): void {
+    this.name = 'Spider-man';
+  }
+
+  changeAge(): void {
+    this.age = 25;
+  }
 }
