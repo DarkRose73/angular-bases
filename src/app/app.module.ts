95a60faa4aaee2f006751a3e6b23aebfee955365
Joan Salas <joan.salas.silva@gmail.com>
2023-05-07 21:28:32 -0400

    heroes-hero & heroes-list components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   // Aqui se declaran los componentes que se utilizaran en este modulo
-  declarations: [AppComponent, CounterComponent],
+  declarations: [AppComponent, CounterComponent, HeroComponent, ListComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
