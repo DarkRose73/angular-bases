1f7bebfa2421f617c950346cb0fe3e33ddc54cf8
Joan Salas <joan.salas.silva@gmail.com>
2023-05-10 12:47:46 -0400

    getting data from parent component

--- src/app/dbz/components/list/list.component.ts
+++ src/app/dbz/components/list/list.component.ts
@@ -1,8 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Character } from '../../interfaces/character.interface';
 
 @Component({
   selector: 'dbz-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.css'],
 })
-export class ListComponent {}
+export class ListComponent {
+  // PARA PERMITIR LA COMUNICACION ENTRE UN COMPONENTE HIJO Y PADRE SE UTILIZA LA PROPERTY @Input()
+  // SE LE PUEDE PASAR COMO PARÁMETRO EL NOMBRE DE LA PROPIEDAD, SINO SE USA EL
+  // MISMO NOMBRE CON EL QUE LLEGA DESDE EL PADRE
+  @Input()
+  public characterList: Character[] = [{ name: 'Trunks', power: 10 }];
+}
